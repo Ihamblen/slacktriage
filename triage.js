0e36a36f1a532963f57887676889c6fee87e6e2c
Ihamblen <ihamblen@vmware.com>
2017-06-13 07:40:14 +0100

    Create triage.js

--- triage.js
+++ triage.js
@@ -40,7 +40,7 @@ function getRequest(settings, message) {
   // flags based on reactions
   let reactions = (message.reactions || []).map(r => r.name);
   let addressed = settings.addressed.emojis.some(e => reactions.includes(e));
-  let pending = emoji && !review && !addressed;
+  let pending = emoji && !addressed;
   
   let id = message.ts.replace('.', '');                       // deep link id
   let bot = message.subtype === 'bot_message';                // bot posts
