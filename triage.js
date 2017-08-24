7c60efab572a5c6a4e49da7485a6ffff42ba9c16
maozmike <31312496+maozmike@users.noreply.github.com>
2017-08-24 16:08:01 +0300

    Update triage.js

--- triage.js
+++ triage.js
@@ -102,9 +102,9 @@ function buildSection(settings, requests, payload, name) {
   // replace template fields
   text = text.replace(/{{count}}/g, filtered.length);*/
   text = text.replace(/{{channel}}/g, `<#${channel_id}|${channel_name}>`);
-  if(text.indexOf(":white_check_mark;")<0 
+  if(text.indexOf(":white_check_mark;")<0{
 
-  return text;
+  return text;}
 }
 
 
